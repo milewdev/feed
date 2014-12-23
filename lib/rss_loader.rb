c4ffdb95471ce864517465431b595a3c23717102
Michael Lewandowski <milewgit@gmail.com>
2014-12-23 00:26:51 -0500

    Refactor: extract method purge_existing_data

--- lib/rss_loader.rb
+++ lib/rss_loader.rb
@@ -2,34 +2,41 @@ require 'rss'
 require 'open-uri'
 
 class RssLoader
-  def load
+    def load
 
-    Channel.destroy_all
+      purge_existing_data()
 
-    url = 'http://www.macleans.ca/multimedia/feed/'
-    open(url) do |rss_io|
-      rss_data = RSS::Parser.parse(rss_io)
-      channel = Channel.create(
-        title:            rss_data.channel.title,
-        link:             rss_data.channel.link,
-        description:      rss_data.channel.description,
-        last_build_date:  rss_data.channel.lastBuildDate,
-        language:         rss_data.channel.language,
-        generator:        rss_data.channel.generator
-      )
-
-      rss_data.items.each do |item|
-        channel.items.create(
-          title:          item.title,
-          link:           item.link,
-          comments:       item.comments,
-          pub_date:       item.pubDate,
-          guid:           item.guid.to_s,
-          description:    item.description
+      url = 'http://www.macleans.ca/multimedia/feed/'
+      open(url) do |rss_io|
+        rss_data = RSS::Parser.parse(rss_io)
+        channel = Channel.create(
+          title:            rss_data.channel.title,
+          link:             rss_data.channel.link,
+          description:      rss_data.channel.description,
+          last_build_date:  rss_data.channel.lastBuildDate,
+          language:         rss_data.channel.language,
+          generator:        rss_data.channel.generator
         )
+
+        rss_data.items.each do |item|
+          channel.items.create(
+            title:          item.title,
+            link:           item.link,
+            comments:       item.comments,
+            pub_date:       item.pubDate,
+            guid:           item.guid.to_s,
+            description:    item.description
+          )
+        end
+
       end
 
     end
 
-  end
+  private
+
+    def purge_existing_data
+      Channel.destroy_all
+    end
+
 end
