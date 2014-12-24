17d3f5792c3bf0ef54ae7d5dadc97595025c44f4
Michael Lewandowski <milewgit@gmail.com>
2014-12-24 13:50:40 -0500

    Refactor: rename #rss_data() to #rss(), #test_data() to #parse(rss)

--- test/unit/rss_loader_test.rb
+++ test/unit/rss_loader_test.rb
@@ -3,30 +3,30 @@ require 'test_helper'
 class RssLoaderTest < Minitest::Test
   def test_that_existing_channel_records_are_deleted
     existing_channel = Channel.create()
-    loader = create_loader_that_loads(rss_data)
+    loader = create_loader_that_loads(rss)
     loader.load
     refute Channel.exists?(existing_channel.id)
   end
 
   def test_that_all_channel_fields_are_loaded
-    loader = create_loader_that_loads(rss_data)
+    loader = create_loader_that_loads(rss)
     loader.load
-    rss_channel, db_channel = test_data.channel, Channel.all.first
+    rss_channel, db_channel = parse(rss).channel, Channel.all.first
     assert_channel_equal rss_channel, db_channel
   end
 
   def test_that_all_existing_item_records_are_deleted
     existing_channel = Channel.create()
     existing_item = existing_channel.items.create()
-    loader = create_loader_that_loads(rss_data)
+    loader = create_loader_that_loads(rss)
     loader.load
     refute Item.exists?(existing_item.id)
   end
 
   def test_that_all_item_fields_are_loaded
-    loader = create_loader_that_loads(rss_data)
+    loader = create_loader_that_loads(rss)
     loader.load
-    rss_items, db_items = test_data.items, Item.all
+    rss_items, db_items = parse(rss).items, Item.all
     assert_items_equal rss_items, db_items
   end
 end
@@ -37,7 +37,7 @@ end
 class RssLoaderTest
   private
 
-  def rss_data
+  def rss
     <<-EOS
       <rss
         xmlns:content="http://purl.org/rss/1.0/modules/content/"
@@ -77,8 +77,8 @@ class RssLoaderTest
     EOS
   end
 
-  def test_data
-    RSS::Parser.parse(rss_data)
+  def parse(rss)
+    RSS::Parser.parse(rss)
   end
 
   def assert_channel_equal(rss_channel, db_channel)
