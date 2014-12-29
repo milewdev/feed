07b71c453a1dff7b19eb01b187af3888c0bb6782
Michael Lewandowski <milewgit@gmail.com>
2014-12-29 09:49:16 -0500

    Convert #expected_json to use data from db

--- test/integration/v1/v1_api_conformance_test.rb
+++ test/integration/v1/v1_api_conformance_test.rb
@@ -43,24 +43,16 @@ class V1ApiConformanceTest
   end
 
   def expected_json
-    <<-EOS.gsub(/\s/, '')
-      [
-        {
-          "id": 1,
-          "title": "title1",
-          "guid": "guid1",
-          "created_at": "2014-12-29T08:43:27.000Z",
-          "updated_at": "2014-12-29T08:43:27.001Z"
-        },
-        {
-          "id": 2,
-          "title": "title2",
-          "guid": "guid2",
-          "created_at": "2014-12-29T08:43:27.002Z",
-          "updated_at": "2014-12-29T08:43:27.003Z"
-        }
-      ]
-    EOS
+    hash_of_db_data = FeedItem.all.map do |item|
+      {
+        id: item.id,
+        title: item.title,
+        guid: item.guid,
+        created_at: to_json_date(item.created_at),
+        updated_at: to_json_date(item.updated_at)
+      }
+    end
+    JSON.generate(hash_of_db_data)
   end
 
   def expected_json2
@@ -70,5 +62,12 @@ class V1ApiConformanceTest
   def get_json_from(url)
     get url
     response.body
+  end
+
+  # 2012-04-23T18:25:43.511Z
+  # See http://stackoverflow.com/a/15952652
+  # TODO: duplicated in end_to_end_test.rb; extract to helper library
+  def to_json_date(date)
+    date.utc.strftime('%FT%T.%LZ')
   end
 end
